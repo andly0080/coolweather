39117b03bb00b6e0d9f79987841a3610bd0abe57
andly0080 <andly0080@gmail.com>
2017-01-09 23:27:42 -0500

    completed select cities, province, county

---
+++ app/src/main/java/com/example/davidyu/coolweather/util/HttpUtil.java
@@ -0,0 +1,21 @@
+package com.example.davidyu.coolweather.util;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by davidyu on 1/9/17.
+ */
+
+public class HttpUtil {
+
+
+    public static void sendOkHttpRequest(String address, okhttp3.Callback callback)
+    {
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(address).build();
+        client.newCall(request).enqueue(callback);
+    }
+
+}
